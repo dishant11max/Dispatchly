de32cc8bb0a634ec31ad31592f08e5a9a4c47fd5
dishant11max <savadiadishan@gmail.com>
2026-01-22 19:36:46 +0530

    scrolling bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import ServicesPage from "@/pages/ServicesPage";
 import HubsPage from "@/pages/HubsPage";
 import AboutPage from "@/pages/AboutPage";
 import DriverLoginPage from "@/pages/DriverLoginPage";
+import ScrollToTop from "@/components/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/services" element={<ServicesPage />} />
